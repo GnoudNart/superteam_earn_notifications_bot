3b750de04d93a95469f712106a894e5bd22f5f64
thanhhv <internet00137@gmail.com>
2025-06-15 17:11:40 +0700

    - update webhookCallback truyen 2 doi so

--- src/bot.ts
+++ src/bot.ts
@@ -112,4 +112,4 @@ bot.api.setMyCommands([
 //     .catch((err) => console.error("Failed to launch bot:", err));
 //
 
-export const webhookHandler = webhookCallback(bot, 'express'))
+export const webhookHandler = webhookCallback(bot, 'express')
